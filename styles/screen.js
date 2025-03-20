e64344432cf54918937a4306b527c5827082328d
ennisjo <ennisjo1@msu.edu>
2025-03-20 03:26:46 +0000

    Attempt 3 Week 8 Work

--- styles/screen.js
+++ styles/screen.js
@@ -25,3 +25,21 @@ window.onload = function(){
 
 }
 
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://hneuegdqofiksjrylcfw.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuZXVlZ2Rxb2Zpa3NqcnlsY2Z3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4MTQ5ODIsImV4cCI6MjA1NzM5MDk4Mn0.APK-aJXdeDoH4SVp_N3G1g3dHuhyIr9SVhvsyjWP1Ek'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+          
+          
+
+for (let book of books){
+    let bookList = document.getElementById('books');
+    bookList.innerHTML += '<li>${book.title}</li>';
+}
+  
